8bb361b0e5a5fe78ce39316c8eb1252c6acf00b6
valdas <valdas.kriunas@panko.lt>
2024-11-10 20:15:07 +0200

    Made some progress on enemies

---
+++ Assets/BatMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+    [SerializeField] private float speed;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+    }
+}
